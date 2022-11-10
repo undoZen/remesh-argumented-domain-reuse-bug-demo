11091c8c4a3b88332673e4b6d5e71e8bfcdfda12
UZEN <opensource@undozen.com>
2022-11-10 13:40:08 +0800

    works after NameAsyncDataQuery been used

--- src/pages/single.tsx
+++ src/pages/single.tsx
@@ -3,14 +3,14 @@ import { SingleDataDomain } from "../domains/single"
 
 export default function Page() {
     const someDomain = useRemeshDomain(SingleDataDomain('zh'))
-    // const asyncData = useRemeshQuery(someDomain.query.NameAsyncDataQuery())
+    const asyncData = useRemeshQuery(someDomain.query.NameAsyncDataQuery())
     const loadedName = useRemeshQuery(someDomain.query.LoadedNameQuery())
     useRemeshEvent(someDomain.event.GotNameEvent, (name) => {
         alert('name successfully loaded: hello, ' + name + '!')
     })
     const nameNameName = useRemeshQuery(someDomain.query.NameThreeTimesQuery())
-    // if (asyncData.type !== 'success') {
-    //     return <p>hello, {loadedName}! loading... please wait</p>
-    // }
+    if (asyncData.type !== 'success') {
+        return <p>hello, {loadedName}! loading... please wait</p>
+    }
     return <p>hello, {loadedName}! - {nameNameName}</p>
 }

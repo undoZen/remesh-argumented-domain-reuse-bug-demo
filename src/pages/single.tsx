ec08f95dcacfc1e47ba1d9c80e88dce1528e6cba
UZEN <opensource@undozen.com>
2022-11-10 13:31:09 +0800

    fix single page

--- src/pages/single.tsx
+++ src/pages/single.tsx
@@ -3,8 +3,8 @@ import { SingleDataDomain } from "../domains/single"
 
 export default function Page() {
     const someDomain = useRemeshDomain(SingleDataDomain('zh'))
-    const asyncData = useRemeshQuery(someDomain.query.NameAsyncDataQuery())
-    // const loadedName = useRemeshQuery(someDomain.query.LoadedNameQuery())
+    // const asyncData = useRemeshQuery(someDomain.query.NameAsyncDataQuery())
+    const loadedName = useRemeshQuery(someDomain.query.LoadedNameQuery())
     useRemeshEvent(someDomain.event.GotNameEvent, (name) => {
         alert('name successfully loaded: hello, ' + name + '!')
     })
